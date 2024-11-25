cbc169be1ae877adcdce61093b3173efc7a3c1a2
root <root@DESKTOP-PMPVG6O>
2024-11-25 17:48:53 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VueRouterAutoImports } from 'unplugin-vue-router';
+import VueRouter from 'unplugin-vue-router/vite';
+
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+	resolve: {
+		alias: {
+			'@/': `${path.resolve(__dirname, 'src')}/`,
+		},
+	},
+	plugins: [
+		vue(),
+
+		// https://github.com/posva/unplugin-vue-router
+		VueRouter(),
+
+		// https://github.com/antfu/unplugin-auto-import
+		AutoImport({
+			imports: [
+				'vue',
+				'@vueuse/core',
+				VueRouterAutoImports,
+				{
+					// add any other imports you were relying on
+					'vue-router/auto': ['useLink'],
+				},
+			],
+			dts: true,
+			dirs: ['./src/composables'],
+			vueTemplate: true,
+		}),
+
+		// https://github.com/antfu/vite-plugin-components
+		Components({
+			dts: true,
+		}),
+	],
+
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: true,
+		host: host || false,
+		hmr: host
+			? {
+					protocol: 'ws',
+					host,
+					port: 1421,
+				}
+			: undefined,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ['**/src-tauri/**'],
+		},
+	},
+}));
